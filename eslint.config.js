4164b6d250c8f19ee8311e3cbdf26e78bd5948dd
Sevenn <83214237+thesevenn@users.noreply.github.com>
2025-04-20 00:28:05 +0530

    chore: update config to allow empty interfaces in components

--- eslint.config.js
+++ eslint.config.js
@@ -5,10 +5,10 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: [ 'dist' ] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [ js.configs.recommended, ...tseslint.configs.recommended ],
+    files: [ '**/*.{ts,tsx}' ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -21,7 +21,7 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        { allowConstantExport: true, allowInterfaces: true },
       ],
     },
   },
