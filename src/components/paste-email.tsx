06a9d1440f96aca7f270d321cf0707411fc01f59
Sevenn <83214237+thesevenn@users.noreply.github.com>
2025-04-24 01:34:19 +0530

    feat: add paste-email.tsx for pasting feature for emails

---
+++ src/components/paste-email.tsx
@@ -0,0 +1,12 @@
+import {FC} from "react";
+
+interface PropType {}
+const PasteEmail: FC<PropType> = () => {
+	return (
+		<div className="flex items-center justify-center h-full">
+			Currently Unavailable
+		</div>
+	);
+};
+
+export default PasteEmail;
