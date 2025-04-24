0d98d5bd34309f75b23acf088b93e4947f5c39c7
Sevenn <83214237+thesevenn@users.noreply.github.com>
2025-04-24 14:44:41 +0530

    feat: add footer.tsx ui and copy

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,14 +1,29 @@
 import {FC} from "react";
 
 import {Separator} from "@/components/ui/separator";
+import PhishookLogo from "../shared/phishook";
 
 interface PropType {}
 const Footer: FC<PropType> = () => {
 	return (
-		<footer className="bg-brand-primary">
-			<div>Footer</div>
+		<footer className="bg-brand-primary px-4 lg:px-24 py-4">
+			<div className="flex flex-col md:flex-row md:items-center gap-6 md:gap-0 md:justify-between py-4">
+				<div className="flex items-center gap-2 md:gap-3">
+					<div className="w-8 lg:w-12 aspect-square text-white">
+						<PhishookLogo />
+					</div>
+					<p className="md:text-xl font-medium">PhisHook</p>
+				</div>
+				<ul className="flex flex-col md:flex-row md:items-center gap-5 md:gap-10">
+					<li>Disclaimer</li>
+					<li>See it in action</li>
+				</ul>
+			</div>
 			<Separator />
-			<div>Dispatch</div>
+			<div className="flex items-center justify-between pt-4">
+				<p>Built by Sevenn</p>
+				<p>Source code - GitHub</p>
+			</div>
 		</footer>
 	);
 };
