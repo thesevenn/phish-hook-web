61355bffa8c0668adccca546cd61c2d734e697ae
Sevenn <83214237+thesevenn@users.noreply.github.com>
2025-04-26 00:16:47 +0530

    chore: add test libs and setup testing env

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import path from "path";
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import {configDefaults} from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	test: {
+		globals: true,
+		setupFiles: "./test-setup.ts",
+		environment: "jsdom",
+		exclude: [...configDefaults.exclude, "e2e/**"],
+	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
