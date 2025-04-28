af22d201b8f41b40266c4db32c159048d1f6cfc3
Sevenn <83214237+thesevenn@users.noreply.github.com>
2025-04-29 01:37:23 +0530

    feat: add skeleton card to show loading card state

---
+++ src/components/skeletons/skeleton-card.tsx
@@ -0,0 +1,19 @@
+import {FC, HTMLAttributes} from "react";
+
+import {cn} from "@/lib/utils";
+
+interface PropType extends HTMLAttributes<HTMLDivElement> {}
+const SkeletonCard: FC<PropType> = ({className, children}) => {
+	return (
+		<div
+			className={cn(
+				"dark:bg-surface-card-dark bg-white max-w-[690px] max-h-[380px] w-full h-full rounded-xl animate-pulse",
+				className
+			)}
+		>
+			{children}
+		</div>
+	);
+};
+
+export default SkeletonCard;
