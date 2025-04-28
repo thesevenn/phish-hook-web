2f879686e0950518de4a1e33968883a7ccef4718
Sevenn <83214237+thesevenn@users.noreply.github.com>
2025-04-29 01:43:29 +0530

    feat: add react-router routes /analyze, /upload to allow multiple route views

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import {BrowserRouter, Routes, Route, Navigate} from "react-router";
+
+import "./index.css";
+import App from "./App.tsx";
+import Verdict from "./components/verdict.tsx";
+import Layout from "./layout.tsx";
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route element={<Layout />}>
+					<Route path="/" element={<Navigate to="/upload" />} />
+					<Route index path="/upload" element={<App />} />
+					<Route path="/verdict" element={<Verdict />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	</StrictMode>
+);
