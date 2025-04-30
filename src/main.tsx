faec497a44e9877388a0859325f13e418da0a92e
Sevenn <83214237+thesevenn@users.noreply.github.com>
2025-04-30 12:34:40 +0530

    fix: move Tooltip provider from components to main to correctly handle open tooltips

--- src/main.tsx
+++ src/main.tsx
@@ -7,17 +7,20 @@ import "./index.css";
 import App from "./App.tsx";
 import Verdict from "./components/verdict.tsx";
 import Layout from "./layout.tsx";
+import {Tooltip} from "@/components/ui/tooltip";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route element={<Layout />}>
-					<Route path="/" element={<Navigate to="/upload" />} />
-					<Route index path="/upload" element={<App />} />
-					<Route path="/verdict" element={<Verdict />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<Tooltip>
+			<BrowserRouter>
+				<Routes>
+					<Route element={<Layout />}>
+						<Route path="/" element={<Navigate to="/upload" />} />
+						<Route index path="/upload" element={<App />} />
+						<Route path="/verdict" element={<Verdict />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</Tooltip>
 	</StrictMode>
 );
