5fc85217c6f406dbfbe400498b8c59f07bdb69b8
Sevenn <83214237+thesevenn@users.noreply.github.com>
2025-05-01 14:43:18 +0530

    refactor: update component with Card component to simplify UI

--- src/components/email-omnibar.tsx
+++ src/components/email-omnibar.tsx
@@ -4,11 +4,12 @@ import UploadEmail from "./upload-email";
 import PasteEmail from "./paste-email";
 import {Separator} from "@radix-ui/react-separator";
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
+import Card from "./ui/card";
 
 interface PropType {}
 const UploadOmniBar: FC<PropType> = () => {
 	return (
-		<div className="max-w-[690px] max-h-[380px] w-full h-full bg-white dark:bg-surface-card-dark rounded-xl border-[1px] border-brand-subtle/50 dark:border-border-subtle/30">
+		<Card className="">
 			<Tabs defaultValue="file" className="w-full h-full p-5 lg:pt-7 lg:px-14">
 				<TabsList className="w-full border-b-border-subtle/40 border-b-[1px] bg-transparent border-solid rounded-none p-0">
 					<TabsTrigger value="file">File</TabsTrigger>
@@ -25,7 +26,7 @@ const UploadOmniBar: FC<PropType> = () => {
 					<PasteEmail />
 				</TabsContent>
 			</Tabs>
-		</div>
+		</Card>
 	);
 };
 
