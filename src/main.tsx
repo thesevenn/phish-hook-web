8e578179796c57923573d03dcc0d53e5b2e719ff
Sevenn <83214237+thesevenn@users.noreply.github.com>
2025-05-01 14:56:41 +0530

    feat: add react-query ClientProvider to use react-query

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 
 import {BrowserRouter, Routes, Route, Navigate} from "react-router";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 import "./index.css";
 import App from "./App.tsx";
@@ -9,18 +10,21 @@ import Verdict from "./components/verdict.tsx";
 import Layout from "./layout.tsx";
 import {Tooltip} from "@/components/ui/tooltip";
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<Tooltip>
-			<BrowserRouter>
-				<Routes>
-					<Route element={<Layout />}>
-						<Route path="/" element={<Navigate to="/upload" />} />
-						<Route index path="/upload" element={<App />} />
-						<Route path="/verdict" element={<Verdict />} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
-		</Tooltip>
+		<QueryClientProvider client={queryClient}>
+			<Tooltip>
+				<BrowserRouter>
+					<Routes>
+						<Route element={<Layout />}>
+							<Route path="/" element={<Navigate to="/upload" />} />
+							<Route index path="/upload" element={<App />} />
+							<Route path="/verdict" element={<Verdict />} />
+						</Route>
+					</Routes>
+				</BrowserRouter>
+			</Tooltip>
+		</QueryClientProvider>
 	</StrictMode>
 );
