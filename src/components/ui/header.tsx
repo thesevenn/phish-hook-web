2ff42a32b6dce478a3f895d5d9ce5a2ed24b2b82
Sevenn <83214237+thesevenn@users.noreply.github.com>
2025-05-03 00:50:40 +0530

    refactor: update header.tsx to use darkMode context replacing darkmode hook to allow global access of state

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -3,20 +3,20 @@ import {FC} from "react";
 import {Sun, Moon} from "lucide-react";
 
 import PhishookLogo from "../shared/phishook";
-import {useDarkMode} from "../../hooks/use-dark-mode";
+import {useDarkModeContext} from "@/contexts/darkmode-context";
 import APIStatus from "../shared/api-status";
 
 interface PropType {}
 const Header: FC<PropType> = () => {
-	const [darkMode, setDarkMode] = useDarkMode();
+	const {darkMode, setDarkMode} = useDarkModeContext();
 	return (
 		<header className="bg-brand-primary px-4 lg:px-24 py-4 flex items-center justify-between sticky top-0 z-10">
 			<div className="w-10 lg:w-14 aspect-square text-white">
 				<PhishookLogo iswhite />
 			</div>
-			<div className="flex items-center gap-4 lg:gap-8">
+			<div className="flex items-center gap-3 lg:gap-6">
 				<APIStatus />
-				<div className="cursor-pointer text-brand-body">
+				<div className="cursor-pointer text-brand-body hover:bg-surface-primary/30 p-2 rounded-xl">
 					{darkMode && <Sun onClick={() => setDarkMode(false)} />}
 					{!darkMode && <Moon onClick={() => setDarkMode(true)} />}
 				</div>
