f03b2ebdb23fc11cb0eb652b8091fd0146db0732
Sevenn <83214237+thesevenn@users.noreply.github.com>
2025-05-03 00:46:29 +0530

    feat: add darkMode context provider to access darkMode state globally

--- src/main.tsx
+++ src/main.tsx
@@ -9,22 +9,25 @@ import App from "./App.tsx";
 import Verdict from "./components/verdict.tsx";
 import Layout from "./layout.tsx";
 import {Tooltip} from "@/components/ui/tooltip";
+import {DarkModeProvider} from "./contexts/darkmode-context.tsx";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<Tooltip>
-				<BrowserRouter>
-					<Routes>
-						<Route element={<Layout />}>
-							<Route path="/" element={<Navigate to="/upload" />} />
-							<Route index path="/upload" element={<App />} />
-							<Route path="/verdict" element={<Verdict />} />
-						</Route>
-					</Routes>
-				</BrowserRouter>
-			</Tooltip>
-		</QueryClientProvider>
+		<DarkModeProvider>
+			<QueryClientProvider client={queryClient}>
+				<Tooltip>
+					<BrowserRouter>
+						<Routes>
+							<Route element={<Layout />}>
+								<Route path="/" element={<Navigate to="/upload" />} />
+								<Route index path="/upload" element={<App />} />
+								<Route path="/verdict" element={<Verdict />} />
+							</Route>
+						</Routes>
+					</BrowserRouter>
+				</Tooltip>
+			</QueryClientProvider>
+		</DarkModeProvider>
 	</StrictMode>
 );
