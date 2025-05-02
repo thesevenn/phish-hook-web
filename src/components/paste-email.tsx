312858a8d47c81f2a61e2b7e20763ec82b1de1a7
Sevenn <83214237+thesevenn@users.noreply.github.com>
2025-05-02 22:23:12 +0530

    fix: update text colors for dark and light modes

--- src/components/paste-email.tsx
+++ src/components/paste-email.tsx
@@ -3,7 +3,7 @@ import {FC} from "react";
 interface PropType {}
 const PasteEmail: FC<PropType> = () => {
 	return (
-		<div className="flex items-center justify-center h-full">
+		<div className="flex items-center justify-center min-h-[256px] text-surface-card-dark dark:text-brand-body">
 			Currently Unavailable
 		</div>
 	);
