367226414b09034ee6a72d7648666879ee64ec0c
Sevenn <83214237+thesevenn@users.noreply.github.com>
2025-05-06 01:13:36 +0530

    feat: add disclaimer to footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -2,6 +2,14 @@ import {FC} from "react";
 
 import {Separator} from "@/components/ui/separator";
 import PhishookLogo from "../shared/phishook";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
 
 interface PropType {}
 const Footer: FC<PropType> = () => {
@@ -15,8 +23,33 @@ const Footer: FC<PropType> = () => {
 					<p className="md:text-xl font-medium">PhisHook</p>
 				</div>
 				<ul className="flex flex-col md:flex-row md:items-center gap-5 md:gap-10 text-sm">
-					<li>Disclaimer</li>
-					<li>See it in action</li>
+					<li>
+						<Dialog>
+							<DialogTrigger className="cursor-pointer">
+								Disclaimer
+							</DialogTrigger>
+							<DialogContent className="text-gray-800 dark:text-gray-100">
+								<DialogHeader>
+									<DialogTitle className="text-gray-800 dark:text-gray-100">
+										Disclaimer
+									</DialogTitle>
+									<DialogDescription>
+										This tool provides automated analysis based on known
+										phishing patterns and lightweight models. It is a proof of
+										concept developed for academic purposes and is not intended
+										as a full-fledged or enterprise-grade security solution.
+										While it can help flag suspicious emails, no detection
+										system is perfect. Mistakes can occur. Always use your
+										judgment and verify critical communications independently
+										before engaging with any email content.
+									</DialogDescription>
+								</DialogHeader>
+							</DialogContent>
+						</Dialog>
+					</li>
+					<li className="cursor-not-allowed text-brand-muted">
+						See it in action
+					</li>
 				</ul>
 			</div>
 			<Separator />
